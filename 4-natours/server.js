1e54f99c54db2890bc83da06a9ba4cd08380d2c1
Apil Adhikari <apiladhikari.dev@gmail.com>
2024-10-28 12:53:19 +0545

    Enviornment Varaibles Setup - Implement different scripts for dirrerent enviornments - for production env: set NODE_ENV=production && nodemon server.js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+console.log(process.env.NODE_ENV);
+console.log(app.get('env'));
 
-const port = 3000;
+const port = 3000 || process.env.PORT;
 app.listen(port, () => {
   console.log(`Our App is running in the port ${port}.`);
 });

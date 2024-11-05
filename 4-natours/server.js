66eca04eddc1296f3b6356cb0af379d7c3a96e6a
Apil Adhikari <apiladhikari.dev@gmail.com>
2024-11-05 20:22:21 +0545

    Connection our DataBase with Express Application - install mongoose library - use mongoose to connect our application with database - handle the promise returned by the .connect() method of mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,36 @@
+// Require MONGOOSE package
+const mongoose = require('mongoose');
+
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+// Importing Express application
 const app = require('./app');
-console.log(process.env.NODE_ENV);
-console.log(app.get('env'));
+// console.log(process.env.NODE_ENV);
+// console.log(app.get('env'));
+
+// DATABASE CONNECTION PROCESS
+// Replace the placeholder with the actual password
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// Connect to DataBase
+mongoose
+  .connect(
+    DB,
+    //   {
+    //   useNewUrlParser: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }
+  )
+  .then(() => {
+    // console.log(con.connections); // Logs all mongoose instance
+    console.log('DataBase connection successful!');
+  });
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {

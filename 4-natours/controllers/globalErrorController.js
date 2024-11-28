9512bd57bc3641acf11f05dbbfb499b66872034a
Apil Adhikari <apiladhikari.dev@gmail.com>
2024-11-28 09:52:44 +0545

    Handle Errors during Devlopment and Production Enviornment

--- 4-natours/controllers/globalErrorController.js
+++ 4-natours/controllers/globalErrorController.js
@@ -1,3 +1,33 @@
+const sendErrorForDevelopmentEnviornment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    messsage: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProductionEnviornment = (err, res) => {
+  // Operational Error: Trusted Errors: Send message to Client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      messsage: err.message,
+    });
+    // Programming Error or other unknown error: Don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💣', err);
+
+    // 2) Send a generic message
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 /** Global Error Hanlder Function
  * * Seprating global error handler function
  */
@@ -10,8 +40,10 @@ module.exports = (err, req, res, next) => {
   // Getting the status: If there is status defined in error then use that error or use 'error' when there is 500 error statusCode as default
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    messsage: err.message,
-  });
+  // Sending Error Message during Development and Production Enviornment:
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDevelopmentEnviornment(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorForProductionEnviornment(err, res);
+  }
 };
